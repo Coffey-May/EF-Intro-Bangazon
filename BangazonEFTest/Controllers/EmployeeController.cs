32172ca5b9a52ad6bced3991aa18a2366148c9d2
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-04-20 15:40:35 -0500

    employee crud start

---
+++ BangazonEFTest/Controllers/EmployeeController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonEFTest.Data;
+using BangazonEFTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BangazonEFTest.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // Our controllers have a reference to our database via the _context field
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            // This queries the Employee table. Using the `Include` method will join the related entities
+            var employees = _context.Employee
+                .Include(e => e.Computer)
+                .Include(e => e.Department)
+                .ToList();
+
+            return View(employees);
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Employee/Create
+        public async Task<ActionResult> Create()
+        {
+            // Entity Framework will always return data from the database in the form of data models.
+            // If data models are not the type of object you want for your view, you can use the `Select` method
+            // EXAMPLE: We don't want a list of Computer and Department Objects here. We want a list of
+            // SelectListItems.
+            var allComputers = await _context.Computer
+                .Select(d => new SelectListItem() { Text = d.Model, Value = d.Id.ToString() })
+                .ToListAsync();
+            var allDepartments = await _context.Department
+                .Select(d => new SelectListItem() { Text = d.Name, Value = d.Id.ToString() })
+                .ToListAsync();
+
+            var viewModel = new EmployeeCreateViewModel();
+
+            viewModel.DepartmentOptions = allDepartments;
+            viewModel.ComputerOptions = allComputers;
+
+            return View(viewModel);
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(EmployeeCreateViewModel employeeViewModel)
+        {
+            try
+            {
+                var employee = new Employee
+                {
+                    FirstName = employeeViewModel.FirstName,
+                    LastName = employeeViewModel.LastName,
+                    Email = employeeViewModel.Email,
+                    DepartmentId = employeeViewModel.DepartmentId,
+                    ComputerId = employeeViewModel.ComputerId,
+                };
+
+                _context.Employee.Add(employee);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            var allComputers = await _context.Computer
+                .Select(d => new SelectListItem() { Text = d.Model, Value = d.Id.ToString() })
+                .ToListAsync();
+            var allDepartments = await _context.Department
+                .Select(d => new SelectListItem() { Text = d.Name, Value = d.Id.ToString() })
+                .ToListAsync();
+
+            var employee = _context.Employee.FirstOrDefault(e => e.Id == id);
+
+            var viewModel = new EmployeeCreateViewModel()
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Email = employee.Email,
+                ComputerId = employee.ComputerId,
+                DepartmentId = employee.DepartmentId,
+                ComputerOptions = allComputers,
+                DepartmentOptions = allDepartments
+            };
+            
+            return View(viewModel);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, EmployeeCreateViewModel employeeViewModel)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var employee = new Employee()
+                {
+                    Id = id,
+                    FirstName = employeeViewModel.FirstName,
+                    LastName = employeeViewModel.LastName,
+                    Email = employeeViewModel.Email,
+                    ComputerId = employeeViewModel.ComputerId,
+                    DepartmentId = employeeViewModel.DepartmentId,
+                };
+
+                _context.Employee.Update(employee);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
